f15c9f296506770554fce2673eb51c6d8021b4ce
Xue Rui <xr2148u@columbia.edu>
2021-04-13 08:02:51 +0000

    add the view of all squirrels

--- sightings/views.py
+++ sightings/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import JsonResponse
 from .forms import SquirrelForm
+from .models import Squirrel
 
 def index(request):
     return render(request, 'sightings/index.html', {})
@@ -17,5 +18,20 @@ def add_squirrel(request):
     context['form']=form
     return render(request, 'sightings/add.html', context)
    
+def update_suqirrel(request):
+    context = {}
+    
+    form = SquirrelForm(request.POST)
+    if form.is_valid():
+        form.save()
+    context['form']=form
+    return render(request, 'sightings/update.html', context)
+
+def all_squirrels(request):
+    squirrels = Squirrel.objects.all()
+    context = {
+        'squirrels': squirrels
+        }
+    return render(request, 'sightings/all.html', context)
 
 # Create your views here.
