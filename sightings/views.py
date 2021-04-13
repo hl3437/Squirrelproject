4311958b0de5b0d558324cfb5ce8bde42ce78861
Xue Rui <xr2148u@columbia.edu>
2021-04-13 08:19:53 +0000

    add update view

--- sightings/views.py
+++ sightings/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .forms import SquirrelForm
 from .models import Squirrel
+from django.shortcuts import get_object_or_404s
 
 def index(request):
     return render(request, 'sightings/index.html', {})
@@ -18,14 +19,16 @@ def add_squirrel(request):
     context['form']=form
     return render(request, 'sightings/add.html', context)
    
-def update_suqirrel(request):
-    context = {}
-    
-    form = SquirrelForm(request.POST)
-    if form.is_valid():
-        form.save()
-    context['form']=form
-    return render(request, 'sightings/update.html', context)
+def update_squirrel(request, Unique_Squirrel_ID):
+    squirrel = get_object_or_404(Squirrel, pk=Unique_Squirrel_ID)
+    if request.method == 'POST':
+        form = SquirrelForm(request.POST, instance=squirrel)
+        if form.is_valid():
+            form.save()
+            return redirect(f'/sightings/{Unique_Squirrel_ID}')
+    else:
+        form = SquirrelForm(instance=squirrel)
+    return render(request, 'sightings/all.html', context)
 
 def all_squirrels(request):
     squirrels = Squirrel.objects.all()
