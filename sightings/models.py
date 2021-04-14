e38c12742a90ddd36620ef2827b5265f3e3096e9
coconutwtr <81719303+coconutwtr@users.noreply.github.com>
2021-04-13 21:51:34 -0400

    Update models.py

--- sightings/models.py
+++ sightings/models.py
@@ -6,112 +6,154 @@ from django.utils.translation import gettext as _
 class Sight(models.Model):
     objects = None
     Longitude = models.FloatField(
-        help_text=_('Longitude of the sight'), )
+        max_length=17,
+        help_text=_('Longitude of the sighting'),
+        blank=False,
+        )
 
     Latitude = models.FloatField(
-        help_text=_('Latitude of the sight'), )
+        max_length=17,
+        help_text=_('Latitude of the sighting'), 
+        blank=False,
+        )
 
     Unique_Squirrel_Id = models.CharField(
-        help_text=_('The unique ID of the squirrel'),
         max_length=25,
-    )
+        help_text=_('The unique ID of the squirrel'),
+        blank=False,
+        )
+    
     AM = 'AM'
     PM = 'PM'
-    SESSION = (
+    
+    SESSION = [
         (AM, 'AM'),
         (PM, 'PM'),
-    )
+    ]
 
     Shift = models.CharField(
-        help_text=_('Whether the sight is in the morning or late afternoon?'),
         max_length=16,
+        help_text=_('Whether the sighting is in the morning or late afternoon?'),
         choices=SESSION,
-        blank=True)
+        blank=False,
+        )
 
     Date = models.DateField(
-        help_text=_('The format is in yyyy-mm-dd'),
-        null=True,
-        blank=True)
+        help_text=_('(Date in the form of yyyy-mm-dd)'),
+        blank=False,
+        )
 
-    Adult = 'Adult'
-    Juvenile = 'Juvenile'
-    Unknown = '?'
-    AGE_CHOICE = (
-        (Adult, 'Adult'),
-        (Juvenile, 'Juvenile'),
+    ADULT = 'Adult'
+    JUVENILE = 'Juvenile'
+    UNKNOWN = '?'
+    
+    AGE_CHOICE = [
+        (ADULT, 'Adult'),
+        (JUVENILE, 'Juvenile'),
         (None, ''),
-        (Unknown, '?'),
-    )
+        (UNKNOWN, '?'),
+    ]
 
     Age = models.CharField(
+        max_length=8,
         help_text=_('Age of the squirrel'),
-        max_length=16,
         choices=AGE_CHOICE,
         blank=True
-    )
+        )
 
-    Black = 'Black'
-    Gray = 'Gray'
-    Cinnamon = 'Cinnamon'
-    COLOR_CHOICE = (
-        (Black, 'Black'),
-        (Gray, 'Gray'),
-        (Cinnamon, 'Cinnamon'),
+    BLACK = 'Black'
+    GRAY = 'Gray'
+    CINNAMON = 'Cinnamon'
+    
+    COLOR_CHOICE = [
+        (BLACK, 'Black'),
+        (GRAY, 'Gray'),
+        (CINNAMON, 'Cinnamon'),
         (None, ''),
-    )
+    ]
 
     Primary_Fur_Color = models.CharField(
-        help_text=_('Fur color'),
         max_length=16,
+        help_text=_('Primary Fur color'),
         choices=COLOR_CHOICE,
         blank=True
-    )
+        )
 
-    Ground_Plane = 'Ground Plane'
-    Above_Ground = 'Above Ground'
-    LOCATION_CHOICE = (
-        (Ground_Plane, 'Ground Plane'),
-        (Above_Ground, 'Above Ground'),
+    GROUND_PLANE = 'Ground Plane'
+    ABOVE_GROUND = 'Above Ground'
+    
+    LOCATION_CHOICE = [
+        (GROUND_PLANE, 'Ground Plane'),
+        (ABOVE_GROUND, 'Above Ground'),
         (None, ''),
-    )
+    ]
 
     Location = models.CharField(
+        max_length=20,
         help_text=_('Location'),
-        max_length=128,
         choices=LOCATION_CHOICE,
         blank=True
-    )
+        )
 
     Specific_Location = models.CharField(
-        help_text=_('Additional notes to the location'),
         max_length=128,
+        help_text=_('Additional notes to the location'),
         blank=True,
-    )
+        )
 
-    Running = models.BooleanField(null=True)
+    Running = models.BooleanField(
+        default=False,
+        )
 
-    Chasing = models.BooleanField(null=True)
+    Chasingg = models.BooleanField(
+        default=False,
+        )
 
-    Climbing = models.BooleanField(null=True)
+    Climbing = models.BooleanField(
+        default=False,
+        )
 
-    Eating = models.BooleanField(null=True)
+    Eating = models.BooleanField(
+        default=False,
+        )
 
-    Foraging = models.BooleanField(null=True)
+    Foraging = models.BooleanField(
+        default=False,
+        )
 
-    Other_Activities = models.BooleanField(null=True)
+    Other_Activities = models.CharField(
+        max_length=100,
+        blank=True,
+        )
 
-    Kuks = models.BooleanField(null=True)
+    Kuks = models.BooleanField(
+        default=False,
+        )
 
-    Quaas = models.BooleanField(null=True)
+    Quaas = models.BooleanField(
+        default=False,
+        )
 
-    Moans = models.BooleanField(null=True)
+    Moans = models.BooleanField(
+        default=False,
+        )
 
-    Tail_Flags = models.BooleanField(null=True)
+    Tail_flags = models.BooleanField(
+        default=False,
+        )
 
-    Tail_Twitches = models.BooleanField(null=True)
+    Tail_twitches = models.BooleanField(
+        default=False,
+        )
 
-    Approaches = models.BooleanField(null=True)
+    Approaches = models.BooleanField(
+        default=False,
+        )
 
-    Indifferent = models.BooleanField(null=True)
+    Indifferent = models.BooleanField(
+        default=False,
+        )
 
-    Runs_From = models.BooleanField(null=True)
+    Runs_from = models.BooleanField(
+        default=False,
+        )
