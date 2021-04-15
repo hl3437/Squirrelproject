8d690139daa3f997ee47ace5da2ba96990d6f3f2
coconutwtr <81719303+coconutwtr@users.noreply.github.com>
2021-04-15 04:33:29 -0400

    Update urls.py

--- sightings/urls.py
+++ sightings/urls.py
@@ -6,10 +6,10 @@ urlpatterns = [
     path('', views.homepage_view),
     path('home', views.homepage_view),
     path('map/', views.map_view),
-    path('list/', views.list_sights),
+    path('sightings/', views.list_sights),
     path('stats/', views.stats_view),
     path('add/', views.add_sights),
-    path('list/<Unique_Squirrel_Id>/', views.update_sights),
+    path('sightings/<Unique_Squirrel_Id>/', views.update_sights),
     path('index/', views.index),
 
 ]
